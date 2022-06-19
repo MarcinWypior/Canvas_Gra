5ce30df5abd63e30c704c669131eae224f1607a6
@Marcin1136 <marcinwypior1990@gmail.com>
2022-06-19 22:46:55 +0200

    Przed dodawaniem postaci

---
+++ JavaScript/script.js
@@ -0,0 +1,130 @@
+var canvas = document.getElementById('game');
+var context = canvas.getContext('2d');
+var grafikaPlatformy = new Image();
+grafikaPlatformy.src = 'platforma.png';
+var platormy = [];
+platormy[0]= new Platforma(0,500,750,80);
+platormy[1]= new Platforma(730,500,750,80);
+platormy[2]= new Platforma(100,250,100,40);
+platormy[3]= new Platforma(240,360,100,40);
+platormy[4]= new Platforma(500,430,100,40);
+platormy[5]= new Platforma(380,180,100,40);
+platormy[6]= new Platforma(730,300,100,40);
+platormy[7]= new Platforma(670,100,100,40);
+platormy[8]= new Platforma(1000,390,100,40);
+platormy[9]= new Platforma(200,70,100,40);
+platormy[10]= new Platforma(1160,320,100,40);
+platormy[11]= new Platforma(910,160,100,40);
+
+var grafikaMonety = new Image();
+grafikaMonety.src = 'moneta.png';
+var monety = [];
+monety[0] = new Moneta(130,190,35,35);
+monety[1] = new Moneta(270,300,35,35);
+monety[2] = new Moneta(530,330,35,35);
+monety[3] = new Moneta(410,120,35,35);
+monety[4] = new Moneta(760,240,35,35);
+monety[5] = new Moneta(700,40,35,35);
+monety[6] = new Moneta(1030,330,35,35);
+monety[7] = new Moneta(230,30,35,35);
+monety[8] = new Moneta(1190,260,35,35);
+
+var przeszkody=[];
+przeszkody[0]= new Przeszkoda(40,170,35,40,5,'grzyb.png')
+przeszkody[1]= new Przeszkoda(420,340,35,40,10,'ogien.png')
+
+
+
+function rysuj()
+{
+    context.clearRect(0,0,canvas.width,canvas.height)
+    rysujPlatformy();
+    rysujMonety();
+    rysujPrzeszkody();
+}
+
+setInterval(rysuj,10);
+
+
+function Platforma (px,py,pszer,pwys)
+{
+    this.x=px;
+    this.y=py;
+    this.szer=pszer;
+    this.wys=pwys;
+}
+
+function Moneta (px,py,pszer,pwys)
+{
+    this.x=px;
+    this.y=py;
+    this.szer=pszer;
+    this.wys=pwys;
+    this.czywidoczna=true;
+}
+
+function Przeszkoda (px,py,pszer,pwys,pzabiera,pnazwa)
+{
+    this.x=px;
+    this.y=py;
+    this.szer=pszer;
+    this.wys=pwys;
+    this.czywidoczna=true;
+    this.zabiera=pzabiera;
+    this.nazwa=pnazwa;
+    this.GrafikaPrzeszkody = new Image();
+    this.GrafikaPrzeszkody.src = pnazwa;
+}
+
+function rysujPlatformy()
+{
+    for( var i =0; i<platormy.length;i++)
+    {
+                context.drawImage(grafikaPlatformy
+                       ,platormy[i].x
+                       ,platormy[i].y
+                       ,platormy[i].szer
+                       ,platormy[i].wys)   
+    }
+}
+
+
+function rysujMonety()
+{
+    for( var i =0; i<monety.length; i++)
+    {
+        if(monety[i].czywidoczna==true)
+        {
+
+        context.drawImage(grafikaMonety
+                           ,monety[i].x
+                           ,monety[i].y
+                           ,monety[i].szer
+                           ,monety[i].wys)   
+        }
+    }
+}
+
+function rysujPrzeszkody()
+{
+    for( var i =0; i<przeszkody.length; i++)
+    {
+        if(przeszkody[i].czywidoczna==true)
+        {
+
+        context.drawImage(przeszkody[i].GrafikaPrzeszkody
+                           ,przeszkody[i].x
+                           ,przeszkody[i].y
+                           ,przeszkody[i].szer
+                           ,przeszkody[i].wys)   
+        }
+    }
+}
+
+
+
+
+// font-family: "Times New Roman", Times, serif;
+/* dsaf*/
+//'SourceCodePro-Medium', ＭＳ ゴシック, 'MS Gothic', monospace
+
